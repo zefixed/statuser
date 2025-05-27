5a77679dc72af851890fc1c2d2d610ab5c783a60
zefixed <gerasenkov.code@mail.ru>
2025-05-27 22:23:53 +0300

    refactor

--- install.sh
+++ install.sh
@@ -30,6 +30,6 @@ sed -i "s|OnUnitActiveSec=5m|OnUnitActiveSec=$between_timeout|g" /etc/systemd/sy
 
 systemctl daemon-reload
 systemctl start statuser.service
-systemctl start statuser.timer}
+systemctl start statuser.timer
 systemctl enable --now statuser.service
 systemctl enable --now statuser.timer
